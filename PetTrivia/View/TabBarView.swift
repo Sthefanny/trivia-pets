c3edf15b24d3b65c0d6c0ab48dea5fde7be0a880
Naomi Nakanishi <naomi.nknsh@gmail.com>
2021-06-25 15:40:45 -0300

    added tab bar and fixed home screen

---
+++ PetTrivia/View/TabBarView.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarView.swift
+//  PetTrivia
+//
+//  Created by Naomi Nakanishi on 25/06/21.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarView()
+    }
+}
