36a0230a46528acce1ec5ac9f0b49defd5bd0d68
Naomi Nakanishi <naomi.nknsh@gmail.com>
2021-06-26 21:47:32 -0300

    almost done w/ first page of wikipet + tab bar linking

---
+++ PetTrivia/View/WikiPetView/WikiPetCardView.swift
@@ -0,0 +1,20 @@
+//
+//  WikiPetCardView.swift
+//  PetTrivia
+//
+//  Created by Naomi Nakanishi on 26/06/21.
+//
+
+import SwiftUI
+
+struct WikiPetCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WikiPetCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        WikiPetCardView()
+    }
+}
---
+++ PetTrivia/View/WikiPetView/WikiPetProgressBarView.swift
@@ -0,0 +1,20 @@
+//
+//  WikiPetProgressBar.swift
+//  PetTrivia
+//
+//  Created by Naomi Nakanishi on 26/06/21.
+//
+
+import SwiftUI
+
+struct WikiPetProgressBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WikiPetProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        WikiPetProgressBar()
+    }
+}
