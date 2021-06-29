684c28d1e128f6c32041f621a44a02a3173e6f39
Naomi Nakanishi <naomi.nknsh@gmail.com>
2021-06-29 19:29:56 -0300

    gitflow-feature-stash: myPetView

---
+++ PetTrivia/View/MyPet/MyPetView.swift
@@ -0,0 +1,20 @@
+//
+//  MyPetView.swift
+//  PetTrivia
+//
+//  Created by Naomi Nakanishi on 29/06/21.
+//
+
+import SwiftUI
+
+struct MyPetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyPetView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyPetView()
+    }
+}
