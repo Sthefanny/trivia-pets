b26e96957b42cab8d8a25b9015da08042e62e7e1
Naomi Nakanishi <naomi.nknsh@gmail.com>
2021-06-29 16:45:40 -0300

    bug fixes

---
+++ PetTrivia/View/GameView/PreGameView.swift
@@ -0,0 +1,20 @@
+//
+//  PreGameView.swift
+//  PetTrivia
+//
+//  Created by Naomi Nakanishi on 29/06/21.
+//
+
+import SwiftUI
+
+struct PreGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PreGameView_Previews: PreviewProvider {
+    static var previews: some View {
+        PreGameView()
+    }
+}
