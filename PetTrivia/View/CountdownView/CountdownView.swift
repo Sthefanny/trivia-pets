a5e0668161b10f3bc5ecb9018aede83cdae80286
Naomi Nakanishi <naomi.nknsh@gmail.com>
2021-07-01 14:23:34 -0300

    re-fixed countdown view

--- PetTrivia/View/CountdownView/CountdownView.swift
+++ PetTrivia/View/CountdownView/CountdownView.swift
@@ -30,13 +30,13 @@ struct PulsatingView: View {
                     NavigationLink(destination: GameView(currentCategory: QuestionCategory(rawValue: selectedCategory.text)!)
                                     .navigationBarHidden(true)
                                     .navigationBarBackButtonHidden(true), isActive: $isScreenActive){}
-                    Circle().fill(circleColor.opacity(0.3)).frame(width: 200, height: 200).scaleEffect(self.animate ? 1 : 0.1)
-                    Circle().fill(circleColor.opacity(0.9)).frame(width: 148, height: 148).scaleEffect(self.animate ? 1 : 0.1)
+                    Circle().fill(circleColor.opacity(0.3)).frame(width: 200, height: 200).scaleEffect(self.animate ? 1 : 0.5)
+                    Circle().fill(circleColor.opacity(0.9)).frame(width: 148, height: 148).scaleEffect(self.animate ? 1 : 0.5)
                     Text("\(timeRemaining)")
                         .font(Font.custom("HelveticaNeue", size: 96))
                         .fontWeight(.medium)
                         .foregroundColor(Color("BlueTextColor"))
-                        .scaleEffect(self.animate ? 1 : 0.1)
+                        .scaleEffect(self.animate ? 1 : 0.5)
                 }
                 .onAppear {
                     DispatchQueue.main.async {
